fce149b3a4c2f3e2c0540248f59a84846a9d1dc4
Fabio Ricali <ricalifabio@gmail.com>
2020-06-08 11:24:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import {define, createExtWebComponent} from 'doz'
+import {define, createDozWebComponent} from 'doz'
 import cmp from './src'
 
 // expose component to global scope
 function register() {
     if (typeof window !== 'undefined') {
         define('your-component-tag', cmp);
-        createExtWebComponent('your-component-tag', cmp, [/*attributes you want observe*/]);
+        createDozWebComponent('your-component-tag', cmp, [/*attributes you want observe*/]);
     }
 }
 
