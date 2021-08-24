0f36040f5ab1cfef1e267e4a7a122258c743edfa
Fabio Ricali <ricalifabio@gmail.com>
2021-08-24 16:30:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cmp from './src'
 // expose component to global scope
 function register() {
     if (typeof window !== 'undefined') {
-        define('your-component-tag', cmp);
+        define(cmp.tag || 'your-component-tag', cmp);
         // Keep in mind to add a prefix like this: 'dwc-' to the name of the component you are going to define, 
         // this to prevent Doz from processing possible Web Components that have the same name as the Doz Components.
         defineWebComponent('dwc-your-component-tag', cmp, [/*attributes you want observe*/]);
